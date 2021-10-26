b6e8ac365a79c011c6b8c4031a1427139fdc6fdf
Edmundo Paulino <manyymoore@gmail.com>
2021-10-26 13:27:00 -0300

    feat: 💩 Initilize vars

---
+++ lib/store.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:mobx/mobx.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+part 'store.g.dart';
+
+class Token = _Token with _$Token;
+
+FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+abstract class _Token with Store {
+  @observable
+  ObservableFuture? token =
+      ObservableFuture(firestore.collection('tokens').get());
+
+  @action
+  void addToken() {
+    firestore
+        .collection('tokens')
+        .add({
+          'user': 'bull',
+          'secret': 'JX82',
+        })
+        .then((value) => print("Token added"))
+        .catchError((error) => print("Failed to add token: $error"));
+  }
+
+  @action
+  void retrieveToken() {
+    firestore.collection('tokens').get().then((value) => print(token));
+  }
+}
---
+++ lib/store.g.dart
@@ -0,0 +1,57 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'store.dart';
+
+// **************************************************************************
+// StoreGenerator
+// **************************************************************************
+
+// ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
+
+mixin _$Token on _Token, Store {
+  final _$tokenAtom = Atom(name: '_Token.token');
+
+  @override
+  ObservableFuture<dynamic>? get token {
+    _$tokenAtom.reportRead();
+    return super.token;
+  }
+
+  @override
+  set token(ObservableFuture<dynamic>? value) {
+    _$tokenAtom.reportWrite(value, super.token, () {
+      super.token = value;
+    });
+  }
+
+  final _$_TokenActionController = ActionController(name: '_Token');
+
+  @override
+  void addToken() {
+    final _$actionInfo =
+        _$_TokenActionController.startAction(name: '_Token.addToken');
+    try {
+      return super.addToken();
+    } finally {
+      _$_TokenActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  void retrieveToken() {
+    final _$actionInfo =
+        _$_TokenActionController.startAction(name: '_Token.retrieveToken');
+    try {
+      return super.retrieveToken();
+    } finally {
+      _$_TokenActionController.endAction(_$actionInfo);
+    }
+  }
+
+  @override
+  String toString() {
+    return '''
+token: ${token}
+    ''';
+  }
+}
